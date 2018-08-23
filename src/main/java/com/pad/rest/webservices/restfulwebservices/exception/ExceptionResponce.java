31b38b5eee7611b738e4eb4f85e51476463bfd3c
Takao <sukisya70@gmail.com>
2018-08-23 18:46:09 +0900

    response handlling

---
+++ src/main/java/com/pad/rest/webservices/restfulwebservices/exception/ExceptionResponce.java
@@ -0,0 +1,28 @@
+package com.pad.rest.webservices.restfulwebservices.exception;
+
+import java.util.Date;
+
+public class ExceptionResponce {
+
+  private Date timestamp;
+  private String message;
+  private String details;
+
+  public ExceptionResponce(Date timestamp, String message, String details) {
+  	super();
+  	this.timestamp = timestamp;
+  	this.message = message;
+  	this.details = details;
+  }
+
+  public Date getTimestamp() {
+  	return timestamp;
+  }
+  public String getMessage() {
+  	return message;
+  }
+  public String getDetails() {
+  	return details;
+  }
+
+}
