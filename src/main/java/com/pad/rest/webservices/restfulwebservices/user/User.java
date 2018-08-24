7421d6762dde9c8abe95c71ccfb0809be7111bbd
Takao <sukisya70@gmail.com>
2018-08-24 17:51:21 +0900

    add swagger

--- src/main/java/com/pad/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/pad/rest/webservices/restfulwebservices/user/User.java
@@ -2,16 +2,23 @@ package com.pad.rest.webservices.restfulwebservices.user;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.util.Date;
 
+@ApiModel(description="All details about the user.")
 public class User {
 
   private Integer id;
 
   @Size(min=2, message="Name should hove atleast 2 characters")
+  @ApiModelProperty(notes="Name should have atleast 2 characters")
   private String name;
 
   @Past
+  @ApiModelProperty(notes="Birth date should be in the past")
   private Date birthDate;
 
   protected User() {
@@ -53,10 +60,5 @@ public class User {
   public String toString() {
   	return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
   }
-
-
-
-
-
 
 }
