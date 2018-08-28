a018e5c788e0b8681048f2c5036c7a70cb9b6ff6
Takao <sukisya70@gmail.com>
2018-08-28 17:59:30 +0900

    relation post

--- src/main/java/com/pad/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/pad/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,9 @@
 package com.pad.rest.webservices.restfulwebservices.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -7,58 +11,74 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 import java.util.Date;
+import java.util.List;
 
 @ApiModel(description="All details about the user.")
+@Entity
 public class User {
 
-  private Integer id;
+	@Id
+	@GeneratedValue
+	private Integer id;
 
-  @Size(min=2, message="Name should hove atleast 2 characters")
-  @ApiModelProperty(notes="Name should have atleast 2 characters")
-  private String name;
+	@Size(min=2, message="Name should hove atleast 2 characters")
+	@ApiModelProperty(notes="Name should have atleast 2 characters")
+	private String name;
 
-  @Past
-  @ApiModelProperty(notes="Birth date should be in the past")
-  private Date birthDate;
+	@Past
+	@ApiModelProperty(notes="Birth date should be in the past")
+	private Date birthDate;
 
-  protected User() {
+	@OneToMany(mappedBy="user")
+	private List<Post> posts;
+	
+	
+	protected User() {
 
-  }
+	}
 
-  public User(Integer id, String name, Date birthDate) {
-  	super();
-  	this.id = id;
-  	this.name = name;
-  	this.birthDate = birthDate;
-  }
+	public User(Integer id, String name, Date birthDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+	}
 
-  public Integer getId() {
-  	return id;
-  }
+	public Integer getId() {
+		return id;
+	}
 
-  public void setId(Integer id) {
-  	this.id = id;
-  }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-  public String getName() {
-  	return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-  	this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public Date getBirthDate() {
-  	return birthDate;
-  }
+	public Date getBirthDate() {
+		return birthDate;
+	}
 
-  public void setBirthDate(Date birthDate) {
-  	this.birthDate = birthDate;
-  }
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
 
-  @Override
-  public String toString() {
-  	return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
-  }
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+	}
 
 }
